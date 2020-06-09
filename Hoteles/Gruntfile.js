89ad0fa20d6e5f002f8940410cee2973182e1b7e
Minerva <limmoranc@gmail.com>
2020-06-09 11:10:58 -0500

    scripts npm 2

---
+++ Hoteles/Gruntfile.js
@@ -0,0 +1,152 @@
+module.exports = function(grunt){
+  require('time-grunt')(grunt);
+  require('jit-grunt')(grunt, {
+    useminPrepare: 'grunt-usemin'
+  });
+
+  grunt.initConfig({
+  	sass: {
+  		dist: {
+  			files: [{
+  				expand: true,
+  				cwd: 'css',
+  				src: ['*.scss'],
+  				dest: 'css',
+  				ext: '.css'
+  			}]
+  		}
+  	},
+    watch: {
+      files: ['css/*.scss'],
+      tasks: ['css']
+    },
+
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: [
+            'css/*.css',
+            '*.html',
+            'js/*.js'
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: './' 
+          }
+        }
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: './',
+          src: 'images/*.{png,gif,jpg,jpeg}',
+          dest: 'dist/'
+        }]
+      }
+    },    
+
+   copy: {
+      html: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: './',
+          src: ['*.html'],
+          dest: 'dist'
+        }]
+      },
+    },
+  
+    clean: {
+      build: {
+        src: ['dist/']
+      }
+    },
+
+    concat: {
+     options: {
+        separator: ';'
+      },
+      dist: {}
+    },
+   
+    uglify: {
+      dist: {}
+    },
+
+    filerev: {
+      options: {
+        encoding: 'utf8',
+        algorithm: 'md5',
+        length: 20
+      },
+
+      release: {
+        files: [{
+          src: [
+            'dist/js/*.js',
+            'dist/css/*.css',
+          ]
+        }]
+      }
+    },
+   
+    useminPrepare: {
+      foo: {
+        dest: 'dist',
+        src: ['index.html','about.html','contacto.html','precios.html']
+      },
+      options: {
+        flow: {
+          steps: {
+            css: ['cssmin'],
+            js: ['uglify']
+          },
+          post: {
+            css: [{
+              name: 'cssmin',
+              createConfig: function(context, block){
+                var generated = context.options.generated;
+                generated.options = {
+                  keepSpecialComments: 0,
+                  rebase: false
+                }
+              }
+            }]
+          }
+        }
+      }
+    },
+    cssmin: {
+      dist: {}
+    },
+    usemin: {
+      html: ['dist/index.html', 'dist/about.html', 'dist/precios.html', 'dist/contacto.html'],
+      options: {
+        assetsDir: ['dist','dist/css', 'dist/js']
+      }
+    }
+
+  });
+  
+  grunt.registerTask('css', ['sass']);
+  grunt.registerTask('default', ['browserSync', 'watch']);
+  grunt.registerTask('img:compress', ['imagemin']);
+  grunt.registerTask('build', [
+   'clean',
+   'copy',    
+   'concat',
+   'imagemin',  
+   'uglify',
+   'filerev',
+   'useminPrepare',
+   'cssmin',   
+   'usemin'
+  ]);
+ /* grunt.registerTask('default', ['build']);*/
+};
---
+++ Hoteles/js/index.js
@@ -0,0 +1,25 @@
+	$(function(){
+		$("[data-toggle='tooltip']").tooltip();
+		$("[data-toggle='popover']").popover();
+		$('.carousel').carousel({
+			interval: 100
+		});
+
+		$('#contacto').on('show.bs.modal',function(e){
+			console.log('el modal se está  mostrando');
+			$('#contactoBtn').removeClass('btn-outline-success');
+			$('#contactoBtn').addClass('btn-primary');
+			$('#contactoBtn').prop('disabled',true);
+		});
+
+		$('#contacto').on('shown.bs.modal',function(e){
+			console.log('el modal se mostró')
+		});
+		$('#contacto').on('hide.bs.modal',function(e){
+			console.log('el modal se oculta')
+		});
+		$('#contacto').on('hidden.bs.modal',function(e){
+			console.log('el modal se ocultó')
+		});
+
+	});
